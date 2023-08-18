15449feabe026e97d525630f1f95874ac82de543
KenCorp <kennethwarui35@gmail.com>
2023-08-18 07:25:01 +0300

    Completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+        # code goes here!
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [list ** 2 for list in int_list]
+    return squared_list
+
+
+
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range (1, 101):
+        if num %3 == 0 and num%5 == 0:
+            print('FizzBuzz')
+        elif num %3 == 0:
+            print('Fizz')
+        elif num% 5 == 0:
+            print('Buzz')
+        else:
+            print(num)
+fizzbuzz()
